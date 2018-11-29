81524a21d0ac6223a7c1e23b49c35c280d8ae01a
Shaun Davis <shaun@formsbyair.com>
2018-11-30 11:49:03 +1300

    Update app.js

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,10 +1,10 @@
 // Initialise FlexSlider for Carousels
 $(window).load(function() {
     $('.flexslider').flexslider({
-    animation: "fade",
-    directionNav: true,
-    slideshowSpeed: 5000,
+    animation: "slide",
+    directionNav: true,    
     animationSpeed: 600,
-    touch: true
+    touch: true,
+    slideshow: false
     });
 });
